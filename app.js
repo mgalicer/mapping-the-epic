bb0615a7c26299f02632f2717146d95ae4bd15c0
marigalicer <marigalicer@gmail.com>
2016-10-14 15:39:12 -0400

    add ejs as view engine

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 24.6571694, lng: 32.4466171},
-    zoom: 4
-  });
-}
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
 var hannos_journey = ["Straits of Gibraltar", "Mehdya, Morocco", "cape cantin", "Im'fout, Morocco", "Oued Draa, Morocco", "Jbel Sirwa", "Ad Dakhla", "Arguin, Mauretania", "Senegal river", "Cape Mesurado, liberia", "Gulf of Guinea", "Cape Three Points, Ghana", "Mount Cameroon, Cameroon", "Corisco bay", "Libreville"]
+
---
+++ public/index.js
@@ -0,0 +1,7 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 24.6571694, lng: 32.4466171},
+    zoom: 4
+  });
+}
