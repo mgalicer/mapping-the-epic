594a26e9370143800d52af819a2716064ac6ad43
marigalicer <marigalicer@gmail.com>
2016-10-15 16:46:08 -0400

    get geocoding to work

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 var express = require('express');
 var app = express();
 var firebase = require("firebase");
-require('dotenv').config();
+var geocoder;
+// require('dotenv').config();
+// geocoder = new google.maps.Geocoder();
 
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.render('index', {data: {"hello": "bye"}});
+  res.render('index', {data: "hello"});
 });
-
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
-var hannos_journey = ["Straits of Gibraltar", "Mehdya, Morocco", "cape cantin", "Im'fout, Morocco", "Oued Draa, Morocco", "Jbel Sirwa", "Ad Dakhla", "Arguin, Mauretania", "Senegal river", "Cape Mesurado, liberia", "Gulf of Guinea", "Cape Three Points, Ghana", "Mount Cameroon, Cameroon", "Corisco bay", "Libreville"]
-
-
--- public/index.js
+++ public/index.js
@@ -1,8 +1,51 @@
 var map;
+var geocoder;
+
+var hannos_journey = {
+  "Straits of Gibraltar": "description",
+  "Mehdya Morocco": "description",
+  "cape cantin": "description",
+  "Imfout": "description",
+  "Morocco": "description",
+  "Oued Draa": "description",
+  "Morocco": "description",
+  "Jbel Sirwa": "description",
+  "Ad Dakhla": "description",
+  "Arguin": "description",
+  "Mauretania": "description",
+  "Senegal river": "description",
+  "Cape Mesurado": "description",
+  "liberia": "description",
+  "Gulf of Guinea": "description",
+  "Cape Three Points": "description",
+  "Ghana": "description",
+  "Mount Cameroon": "description",
+  "Cameroon": "description",
+  "Corisco bay": "description",
+  "Libreville": "description",
+  "test":"test"
+}
+
+var loc_results = results[0].geometry.location;
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'),
+  {
     center: {lat: 24.6571694, lng: 32.4466171},
     zoom: 4
   });
+
+  geocoder = new google.maps.Geocoder();
+  for(var key in hannos_journey){
+    console.log(key)
+    geocoder.geocode( { 'address': key }, function(results, status) {
+      if (status == 'OK') {
+
+      } else {
+        console.log("key " + key + "was not successful")
+      }
+
+    });
+  }
 }
-console.log()
+
